a63cc3892b57960de806381bde30d222cbdeff34
acme <acme@acmecia.com>
2023-10-18 17:26:28 -0500

    template done

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { FcBarChart } from 'react-icons/fc'
-import { DASHBOARD_SIDEBAR_LINKS } from '../lib/consts/navigation'
-import { Link } from 'react-router-dom'
+import { DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS } from '../lib/consts/navigation'
+import { Link, useLocation } from 'react-router-dom'
+import classNames from 'classnames'
+import { HiOutlineLogout } from 'react-icons/hi'
+const linkClasses =
+	'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
+
 
 export default function Sidebar() {
   return (
@@ -11,22 +16,33 @@ export default function Sidebar() {
             <span className='text-neutral-100 text-lg'>Dashboard</span>
         </div>
 
-        <div className='flex-1'>
+        <div className='flex-1 py-8 flex flex-col gap-0.5'>
             {DASHBOARD_SIDEBAR_LINKS.map((item) => (
                 <SidebarLink key={item.key} item={item}/>
             
             ))
             }
         </div>
-        <div>Bottom part</div>
+        <div className='flex flex-col gap-0.5 pt-2 border-t border-neutral-700'>
+            {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map(item=> (
+            <SidebarLink key={item.key} item={item}/>
+        ))}
+
+            <div className={classNames('text-red-500 cursor-pointer', linkClasses) }>
+            <span className='text-xl'><HiOutlineLogout/></span>
+            Logout
+            </div>
+
+        </div>
     </div>
   )
 }
 
 
 function SidebarLink({item}){
+    const {pathname} = useLocation()
     return(
-        <Link to={item.path}>
+        <Link to={item.path} className={classNames(pathname === item.path ? 'bg-neutral-700 text-white': 'text-neutral-400', linkClasses) }>
         <span className='text-xl'>{item.icon}</span>
         {item.label}
         </Link>
